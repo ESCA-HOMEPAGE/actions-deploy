20a8798713728bffe3c431cd557fb12ba3387eae
RinSabbit <celena1004dev@gmail.com>
2022-09-06 14:59:53 +0900

    init

---
+++ src/main/java/com/deploy/escadeploy/EscaDeployApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.escadeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscaDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscaDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deploy/escadeploy/EscaDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.escadeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscaDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
